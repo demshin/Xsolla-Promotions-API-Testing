19443a0329fb17450bbc07f424c5beafe4b7670a
Aleksandr Demshin <demshin@gmail.com>
2017-06-02 17:31:15 +0500

    + тест getThePromotion

--- src/test/java/io/github/demshin/tests/PromotionTests.java
+++ src/test/java/io/github/demshin/tests/PromotionTests.java
@@ -1,33 +1,51 @@
 package io.github.demshin.tests;
 
-import io.github.demshin.configuration.TestsConfig;
 import io.github.demshin.models.NewPromotionResponce;
 import io.github.demshin.models.Promotion;
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import static io.github.demshin.configuration.TestsConfig.getConfig;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.testng.Assert.assertTrue;
 
 public class PromotionTests extends BaseTest {
+    private String merchantId;
+    private String apiKey;
+    private Promotion promotion;
+
+    private NewPromotionResponce newPromotionResponce;
+
+    private int id;
+
+    public PromotionTests() {
+        merchantId = getConfig().getMerchantId();
+        apiKey = getConfig().getApiKey();
+        promotion = Promotion.getRandomPromotion();
+    }
 
     @Test(description = "Create a new promotion")
+    @BeforeMethod
     public void createNewPromotion() {
-        String merchantId = TestsConfig.getConfig().getMerchantId();
-        String apiKey = TestsConfig.getConfig().getApiKey();
-
-        Promotion promotion = Promotion.getRandomPromotion();
-
-        NewPromotionResponce newPromotionResponce =
+        newPromotionResponce =
                 given().auth().basic(merchantId, apiKey)
                         .contentType(ContentType.JSON).accept(ContentType.JSON).body(promotion)
                         .when().post("").as(NewPromotionResponce.class);
 
         assertTrue(newPromotionResponce.getId() > 0);
+        id = newPromotionResponce.getId();
     }
 
-    @Test
+    @Test(description = "Get the promotion by id")
     public void getThePromotion() {
+        RestAssured.basePath = RestAssured.basePath + "/" + id;
 
+        given().auth().basic(merchantId, apiKey)
+                .contentType(ContentType.JSON).accept(ContentType.JSON)
+                .when().get()
+                .then().statusCode(200).body("id", equalTo(id));
     }
 }
